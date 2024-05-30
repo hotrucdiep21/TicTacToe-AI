93d6579861f4e57d2a594b8bc1fe88f604829bce
hotrucdiep21 <hotrucdiep210203@gmail.com>
2024-05-30 13:59:31 +0700

    complete game board

---
+++ tictactoe.py
@@ -0,0 +1,63 @@
+import sys
+import pygame
+import numpy as np
+
+from constants import*
+
+#py game setup
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TIC TAC TOE AI")
+screen.fill(BG_COLOR)
+
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((ROWS, COLS))
+
+    def mark_sqr(self, row, col, player):
+        self.squares[row][col] = player
+
+    def empty_sqr(self, row, col):
+        return self.squares[row][col] == 0
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.player = 1
+        self.show_lines()
+
+    def show_lines(self):
+        #vertical
+        pygame.draw.line(screen, LINE_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (WIDTH-SQSIZE, 0), (WIDTH-SQSIZE, HEIGHT), LINE_WIDTH)
+        #horizontal
+        pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT-SQSIZE), (WIDTH, HEIGHT-SQSIZE), LINE_WIDTH)
+
+    def next_turn(self):
+        self.player = self.player%2+1 
+def main():
+
+    #object
+    game = Game()
+    board = game.board
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+                pos = event.pos
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
+                print(row, col)
+
+                if board.empty_sqr(row, col):
+                    board.mark_sqr(row, col, game.player)
+                    print(board.squares)
+                    game.next_turn()
+
+        pygame.display.update()
+main()
